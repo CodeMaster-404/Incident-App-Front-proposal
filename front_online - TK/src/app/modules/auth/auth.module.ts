67b86a430405d2934614c6b66173e6b0205756e3
CodeMaster-404 <74175097+CodeMaster-404@users.noreply.github.com>
2021-02-02 11:28:19 +0200

    Add files via upload

---
+++ front_online - TK/src/app/modules/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { HomescreenComponent } from './homescreen/homescreen.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CoreModule } from 'src/app/core/core.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+@NgModule({
+  declarations: [AuthComponent, LoginComponent, RegisterComponent, HomescreenComponent],
+  imports: [
+    CommonModule,
+    CoreModule,
+    AuthRoutingModule,
+    SharedModule
+  ]
+})
+export class AuthModule { }
